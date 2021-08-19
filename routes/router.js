7daaa0ea670bb2b69d0311d16d686581b8ed6d42
Julio Bermudez <jbermudez@distritomoda.com.ar>
2021-08-19 16:57:44 -0300

    secoond

--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Pool = require("../lib/pool");
-
-router.get("/", (req, res) => {
-  Pool.getConnection((error, connection) => {
-    if (error) throw error;
-    let q = ""
-    connection.query(q,(error,rows,fields)=>{
-        if (error) throw error;
-        res.send({data:rows})
-    })
-    connection.release()
-  });
+let getDevices = require('usb-barcode-scanner').getDevices;
+let UsbScanner = require('usb-barcode-scanner').UsbScanner;
+ 
+let scanner = new UsbScanner({
+  vendorId: 34952,
+  productId: 57362,
 });
-
+ 
+scanner.on('data',(data)=>{
+  console.log(data) 
+})
+ 
+scanner.startScanning();
 module.exports = router;
